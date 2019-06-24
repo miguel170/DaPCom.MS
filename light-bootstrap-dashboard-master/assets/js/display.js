6dd0e902ac5bc2ecb3e4d6b249458df1a0bdcc8a
Emmanuel Callado <frances_callado@dlsu.edu.ph>
2019-06-24 21:10:15 +0800

    Updated tweaks
    Refined the Sidebar, Navbar, and Footer into <iframes> for easier debugging

--- light-bootstrap-dashboard-master/assets/js/display.js
+++ light-bootstrap-dashboard-master/assets/js/display.js
@@ -1,11 +1,48 @@
+var Date = new Date();
+
+var getDate = Date.getMonth() + 1 + "-" + Date.getDate() + "-" + Date.getFullYear();
+
 $(document).ready(function() {
     $('.reveal').hide();
     
     $('#submit-act').click(function(){
-        $(form).attr('text', readonly);
-        $('.reveal').slideDown(5000);
+        $('.prerequisite').attr('readonly', true);
+        $('.reveal').slideDown(2000);
+        $('.pdi-sheet').hide();
     });
     
+    $('#new').click(function(){
+        $('.pdi-sheet').slideDown(2000);
+        $(".pdi-sheet, input[type=text], textarea").val("");
+    });
+    
+    $('#row').click(function(){
+        $('.pdi-sheet').slideDown(2000);
+        $(".pdi-sheet").attr('readonly', true);
+    });
+    
+     $('#save').click(function(){
+        $('.pdi-sheet').slideUp(2000);
+         addInventory();
+    });
+    
+    $('#cancel').click(function(){
+        $('.pdi-sheet').slideUp(2000);
+    });
+    
+    
+    function addInventory() {
+        
+  $("#invTable").append(
+      "<tr id = 'row'>" +
+        "<th>New Element</td>" +
+        "<td>"+ getDate + "</td>" +
+        + "<td>"+ getDate + "</td>" +
+         "<td class ='text-success'>Active</td>" +
+        " <td><button class = 'btn btn-danger btn-fill' href='#remove'>Remove</button></td>"+
+      "</tr>"
+  );
+}
     
     
 });
